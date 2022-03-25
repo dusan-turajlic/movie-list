9fc8eb44f26466f30956a504ee7ad722a5f46a6d
Dusan Turajlic <dusan@turajlic.fi>
2022-03-26 01:11:41 +0200

    Sets up the databse and inital connection

--- app/main.py
+++ app/main.py
@@ -1,63 +1,19 @@
-from enum import Enum
+import time
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.get('/items')
+async def index():
+    return [{}]
--- docker/docker-cmd.py
+++ docker/docker-cmd.py
@@ -8,15 +8,13 @@ print(STAGE)
 DEFAULT_HOST = "0.0.0.0"
 
 startup_cmd = ["uvicorn", "app.main:app", "--host", DEFAULT_HOST]
-pip_install_cmd = ["pip", "install", "-r", "requirements.txt"]
 
 if STAGE == "development":
     startup_cmd.append("--reload")
 else:
     startup_cmd.append("--port")
     startup_cmd.append("80")
+    startup_cmd.append("--proxy-headers")
 
-print("installing dependencies")
-subprocess.call(pip_install_cmd)
 print("starting server")
 subprocess.call(startup_cmd)
