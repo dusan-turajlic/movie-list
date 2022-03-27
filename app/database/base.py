95378bd58fcc997034d1d49b83dce5996d0fb45d
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2022-03-28 01:16:18 +0300

    Fixes pipline run issues (#4)

--- app/database/base.py
+++ app/database/base.py
@@ -3,11 +3,11 @@ from os import getenv
 from sqlalchemy import create_engine
 
 SQLALCHEMY_DATABASE_URL = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
-    user=getenv('DB_USERNAME', 'postgres'),
-    password=getenv('DB_PASSWORD', 'postgres'),
+    user=getenv('DB_USERNAME', 'movielist'),
+    password=getenv('DB_PASSWORD', 'movielist'),
     host=getenv('DB_HOST', 'postgres'),
     port=getenv('DB_PORT', '5432'),
-    database=getenv('DB_DATABASE', 'postgres-db')
+    database=getenv('DB_DATABASE', 'movielist')
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
